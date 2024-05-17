0029b92cfc25d3ef6eea5500f81bb712ba175f21
Abi Caleb <sibijoshuva@gmail.com>
2024-05-17 11:40:51 +0530

    add new file

---
+++ MYlearn/condition.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+    int a=12;
+    int b=9;
+    
+    if (a == b)
+    {
+        printf("true");
+    }
+    else if (a < b)
+    {   
+        printf("this line");
+    }
+    else 
+    
+        printf("false");
+    
+}
