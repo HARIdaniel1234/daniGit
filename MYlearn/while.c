cb472c45c81aa8c57ec89794bd527ed7c1a2ed2b
Abi Caleb <sibijoshuva@gmail.com>
2024-05-22 15:40:59 +0530

    add new file

---
+++ MYlearn/while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// 22/05/2024
+
+int main ()
+{
+    int i = 1,n,sum=1;
+
+    printf("Enter tha value of n :");
+    scanf("%d",&n);
+
+    while(i<=n)
+    {
+        sum=sum=i;
+        printf("%d",n);
+    }
+
+    printf("sum = %d",n);
+
+}
