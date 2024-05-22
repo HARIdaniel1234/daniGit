862b43e93ffd7612bb82f3b4ca4e2215beeede7f
Abi Caleb <sibijoshuva@gmail.com>
2024-05-22 15:34:46 +0530

    add new file

---
+++ MYlearn/forloop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+// 20/05/2024
+
+void main ()
+{
+    int i,n, sum =0;
+
+printf("Enter tha value of n : ");
+scanf("%d",&n);
+
+for(i=1; i<=n; i++);
+{
+    sum=sum+i;
+   
+    
+
+}   
+
+ printf("sum = %d",sum);
+
+}
